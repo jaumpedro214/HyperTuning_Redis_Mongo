4cdb07eb0a903d6061f94c6529f4eba7d62fe0f6
joao pedro da silva lima <joaopedrodasilvalima@gmail.com>
2021-04-21 01:58:34 -0300

    Implemented model training

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import redis
+import numpy as np
 
 from sklearn.linear_model import LinearRegression, Ridge
 from sklearn.ensemble import RandomForestRegressor
+
 from sklearn.metrics import get_scorer
 from sklearn.metrics import SCORERS
 
-from pprint import pprint
+from sklearn.datasets import make_regression, load_boston, load_diabetes
+from sklearn.datasets import make_classification, load_iris, load_breast_cancer
+from sklearn.model_selection import train_test_split, cross_validate
 
-# Sklearn models mapped by name
-models = { "LinearRegression":LinearRegression(),
-           "Ridge":Ridge(),
-           "RandomForestRegressor":RandomForestRegressor() }
-           
+from pprint import pprint
 
 # Function to convert string into int, float, string
 def convert_string(s):
@@ -25,8 +25,29 @@ def convert_string(s):
             pass
     return s
 
-def train_model( params ):
+def get_model(name):
+    # Sklearn models mapped by name
+    models = { "LinearRegression":LinearRegression(),
+               "Ridge":Ridge(),
+               "RandomForestRegressor":RandomForestRegressor() }
+    return models[name]
 
+def train_model(model, metrics, base):
+    X, y = datasets[base]
+
+    scorers = dict()
+    for metric in metrics:
+        scorers[metric] = metric
+
+    # Cross validation cv = 3
+    cvs = cross_validate(model, X, y, return_train_score=True,
+                         scoring=scorers, cv=4, verbose=1)
+
+    cvs_means = { key:np.mean(value) for key,value in cvs.items() }
+    pprint(cvs)
+    pprint(cvs_means)
+
+def create_model( params ):
     assert type(params) == dict
     if params == {}:
         print("Warning: Params dictonary is empty, returning")
@@ -34,7 +55,7 @@ def train_model( params ):
     
     name = params['name']
     base = params['base']
-    metrics = params['metrics'].split(',')
+    metrics = set(params['metrics'].split(','))
 
     del params['name']
     del params['base']
@@ -42,8 +63,8 @@ def train_model( params ):
 
     print( f"Training {name} on base {base}" )
 
-    # Setting ML model 
-    model = models[name]
+    # Setting ML model
+    model = get_model(name)
 
     # Setting model parameters
     for key, value in params.items():
@@ -51,15 +72,20 @@ def train_model( params ):
     model.set_params(**params)
 
     print( f"Evaluating with {metrics}" )
-    
-    # Getting scorers (metrics) objects
-    scorers = []
-    for metric in metrics:
-        scorers.append( get_scorer(metric) )
 
     pprint( model )
     print(" ")
+
+    # Training model
+    train_model(model, metrics, base)
     
+# Available Datasets
+datasets = {"diabetes":load_diabetes(return_X_y=True),
+            "boston":load_boston(return_X_y=True),
+            "iris":load_iris(return_X_y=True),
+            "cancer":load_breast_cancer(return_X_y=True)
+            }
+
 # Connecting to redis client
 r = redis.Redis(decode_responses=True)
 
@@ -69,9 +95,9 @@ while True:
     if id_requisicao == None:
         print("Shutdown...")
         break
-    id_requisicao = int(id_requisicao[1])
+    id_requisicao = id_requisicao[1]
 
-    print(f"Atendendo à requisição {id_requisicao}")
+    print(f"Requisition ID - {id_requisicao}")
     params = r.hgetall(id_requisicao)
-    train_model( params )
+    create_model( params )
     r.delete(id_requisicao)
--- requisitions.py
+++ requisitions.py
@@ -1,6 +1,9 @@
 import redis
+import uuid
 
-def insert_requisition( redis_client, hname, params ):
+def insert_requisition( redis_client, params ):
+    hname = str(uuid.uuid1()) # Creating UUID
+    print(hname)
     for key, value  in params.items():
         redis_client.hset(hname, key, value)
     
@@ -9,20 +12,19 @@ def insert_requisition( redis_client, hname, params ):
 
 redis_client = redis.Redis()
 
-models = [('10', {"name":"RandomForestRegressor", 
-                  "base":"iris",
-                  "min_samples_split":10, "min_impurity_decrease":1.0000 , 
-                  "metrics":"neg_mean_absolute_error"}),
-          ('11', {"name":"RandomForestRegressor",
-                  "base":"iris", 
-                  "n_estimators":200, "min_samples_split":4, "criterion":"mse", 
-                  "metrics":"neg_mean_squared_error,r2"}),
-          ('12', {"name":"Ridge",
-                  "base":"iris", 
-                  "alpha":0.5, "tol":0.0001,
-                  "metrics":"r2"
-                   })]
+models = [{"name":"RandomForestRegressor", 
+           "base":"boston",
+           "min_samples_split":10, "min_impurity_decrease":1.0000, 
+           "metrics":"neg_mean_absolute_error"},
+          {"name":"RandomForestRegressor",
+           "base":"boston", 
+           "n_estimators":200, "min_samples_split":4, "criterion":"mse", 
+           "metrics":"neg_mean_squared_error,r2"},
+          {"name":"Ridge",
+           "base":"boston", 
+           "alpha":0.5, "tol":0.001,
+           "metrics":"r2"}
+           ]
 
 for model in models:
-    print(model[0], model[1])
-    insert_requisition( redis_client, model[0], model[1] )
+    insert_requisition( redis_client, model )
